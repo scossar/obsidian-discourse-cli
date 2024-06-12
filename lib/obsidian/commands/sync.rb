6f2c06c6af6ff97a954dfb48ee2e7fe7d4c0e34a
scossar <scossar3@gmail.com>
2024-06-11 23:39:41 -0700

    use DirectoryUtils

--- lib/obsidian/commands/sync.rb
+++ lib/obsidian/commands/sync.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require 'fileutils'
 require 'obsidian'
+require_relative '../../directory_utils'
 
 module Obsidian
   module Commands
     class Sync < Obsidian::Command
       def call(_args, _name)
-        dir = vault_dir
+        dir = DirectoryUtils.vault_dir
       rescue StandardError => e
         rescue_from_error(e)
       end
@@ -17,38 +17,6 @@ module Obsidian
       end
 
       private
-
-      def vault_dir
-        answer = ''
-        loop do
-          answer = CLI::UI::Prompt.ask("Vault root directory (enter 'q' to quit)",
-                                       is_file: true).strip
-          exit if answer.downcase == 'q'
-
-          if answer.empty?
-            message = 'The directory path cannot be empty. Please try again'
-            user_error(message)
-            next
-          end
-
-          expanded_answer = File.expand_path(answer)
-          unless File.directory?(expanded_answer)
-            message = "The directory '#{answer}' does not exist. Please try again."
-            user_error(message)
-            next
-          end
-
-          confirm = CLI::UI::Prompt.confirm("Is '#{answer}' correct?")
-          break if confirm
-        end
-        answer
-      end
-
-      def user_error(message)
-        CLI::UI::Frame.open('Error') do
-          puts message
-        end
-      end
 
       def rescue_from_error(error)
         CLI::UI::Frame.open('Error') do
