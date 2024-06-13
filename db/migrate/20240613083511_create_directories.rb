cf2f6064acb26f455fdf51a47a373a2b8a55c7fb
scossar <scossar3@gmail.com>
2024-06-13 01:36:42 -0700

    create table migrations

---
+++ db/migrate/20240613083511_create_directories.rb
@@ -0,0 +1,4 @@
+class CreateDirectories < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
---
+++ db/migrate/20240613083525_create_discourse_categories.rb
@@ -0,0 +1,4 @@
+class CreateDiscourseCategories < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
