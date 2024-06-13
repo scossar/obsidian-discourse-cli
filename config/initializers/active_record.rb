4e435f98f2051c0cc3f985b78c11d6f4e3d2ce8a
scossar <scossar3@gmail.com>
2024-06-13 14:18:15 -0700

    establish a db connection before the app loads anything else

---
+++ config/initializers/active_record.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'active_record'
+require 'yaml'
+
+db_config = YAML.load_file('config/database.yml')
+ActiveRecord::Base.establish_connection(db_config['development'])
