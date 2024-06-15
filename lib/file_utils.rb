eac2828e6ea4e2ca524315644ac828d442da6f68
scossar <scossar3@gmail.com>
2024-06-15 11:17:46 -0700

    don't raise error from title_from_file_path method

--- lib/file_utils.rb
+++ lib/file_utils.rb
@@ -3,8 +3,6 @@
 module Obsidian
   module FileUtils
     def self.title_from_file_path(file_path)
-      rails ArgumentError, 'Invalid file extension' unless file_path.end_with?('.md')
-
       File.basename(file_path, '.md')
     end
   end
